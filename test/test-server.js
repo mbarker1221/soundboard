db502a053c51a4a84e7bd3ab032772af19816b7c
michelle barker <mbarker1221@gmail.com>
2018-02-06 01:28:55 -0500

    new files added

---
+++ test/test-server.js
@@ -0,0 +1,28 @@
+
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+const {app, runServer, closeServer} = require('server.js');
+
+const expect = chai.expect;
+
+chai.use(chaiHttp);
+
+describe('call', function() {
+
+  before(function() {
+    return runServer();
+  });
+
+  after(function() {
+      return closeServer();
+  });
+
+  it('should indicate a call to the server was initiated on GET', function() {
+     return chai.request(app)
+      .get('/')
+      .then(function(res) {
+        expect(res).to.have.status(200);
+      });
+    });
+  });
