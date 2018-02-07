e44219a14de333ff26e47f40a2ad646f122e78aa
michelle barker <mbarker1221@gmail.com>
2018-02-07 02:14:46 -0500

    added tests

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
 const app = express();
 app.use(express.static('public'));
+
+app.all('/', (req, res) => res.status(201).send('ok'));;
 app.listen(process.env.PORT || 8080);
