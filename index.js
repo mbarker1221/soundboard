fd45ad977df2fc53a546890fd2e1eed9ab2e2915
michelle barker <mbarker1221@gmail.com>
2018-02-09 03:15:27 -0500

    updates

--- index.js
+++ index.js
@@ -1,43 +1,88 @@
+'use strict';
+
+const bodyParser = require('body-parser');
 const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+const {DATABASE_URL, PORT} = require('/config.js');
+const {Events} = require('/models.js');
+
 const app = express();
+
+app.use(morgan('common'));
+app.use(bodyParser.json());
 app.use(express.static('public'));
+
+app.get('/events', (req, res) => {
+  Events
+    .find()
+    .then(events => {
+      res.json(event.map(events => event.serialize()));
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({error: 'error'});
+    });
+});
+  id: uuid.v4(),
+  title: {type: String, required: true},
+  venue_name: {type: String, required: true},
+  start_date: {type: Date, default: Date.now},
+  city_name: {type: String, required: true},
+  description: {type: String, required: true},
+  url: {type: url}
+
+
 
 const MOCK_EVENTS = {
 	"events": [
-
 	{ 
-	 "name": "rock show",
-     "artist" : "rock star",
-	 "date" : "February 25, 2018",
-	 "venue": "stadium"
+	 "id": "123",
+	 "title": "Rock Star",
+     "venue_name" : "this place",
+	 "start_date" : "2018-23-02, 19:00:00",
+	 "city_name": "Atlanta",
+	 "description": "An all ages show with music",
+	 "url": "www.RockStar@eventful.com"
 	},
 
 	{
-	 "name": "rap show",
-	 "artist": "rap star",
-	 "date": "February 29, 2018",
-	 "venue": "civic center"
+	  "id": "456",
+	 "title": "Rap Star",
+     "venue_name" : "this place",
+	 "start_date" : "2018-25-02, 19:00:00",
+	 "city_name": "Atlanta",
+	 "description": "An all ages show with music",
+	 "url": "www.rapStar@eventful.com"
 	},
 
 	{
-	 "name": "country show",
-	 "artist": "country singer",
-	 "date": "March 24, 2018",
-	 "venue": "honky tonk bar"
+	  "id": "789",
+	 "title": "Country Star",
+     "venue_name" : "that place",
+	 "start_date" : "2018-27-02, 19:00:00",
+	 "city_name": "Atlanta",
+	 "description": "An all ages show with music",
+	 "url": "www.CountryStar@eventful.com"
 	},
 	
 	{
-	 "name": "electronic music show",
-	 "artist": "dj killer",
-	 "date": "April 02, 2018",
-	 "venue": "goat farm"
+	  "id": "012",
+	 "title": "Electronic Star",
+     "venue_name" : "the other place",
+	 "start_date" : "2018-20-02, 19:00:00",
+	 "city_name": "Atlanta",
+	 "description": "An all ages show with music",
+	 "url": "www.danceYourAssOff@eventful.com"
 	}
   ]
 };
 
-function getEvents(callbackF) {
+function getEvents(callback) {
 setTimeout(function() { 
-	callbackF(MOCK_EVENTS)}, 2);
+	callback(MOCK_EVENTS)}, 2);
 }
 
 function displayEvents(data) {
@@ -46,7 +91,6 @@ function displayEvents(data) {
 			'<p>' + data.Event[index].text + '<p>');
 	}
 }
-
 function renderEvents() {
 	getEvents(displayEvents);
 }
--- server.js
+++ server.js
@@ -1,5 +1,25 @@
+
+'use strict';
+
+const bodyParser = require('body-parser');
+const express = require('express');
+const morgan = require('morgan');
+
+
+
+const {Events} = require('/models.js');
+
+const app = express();
+
+app.use(morgan('common'));
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+app.use('/events', pageRouter.js);
+
+
 function showEvents() { 
-	var oArgs = {
+  var oArgs = {
          app_key:"c7nd5jGWK8tkcThz",            
          id: "20218701",
          page_size: 25 ,
@@ -10,7 +30,7 @@ function showEvents() {
 }
 
 function searchShows() {
-	var oArgs = {
+  var oArgs = {
       app_key: "c7nd5jGWK8tkcThz",
       q: "music",
       where: "Atlanta", 
@@ -22,3 +42,47 @@ function searchShows() {
  EVDB.API.call("/events/search", oArgs, function(oData) {   
     });
 }
+
+
+let server;
+
+function runServer(url, port = PORT) {
+  return new Promise((resolve, reject) => {
+    mongoose.connect(databaseUrl, err => {
+      if (err) {
+        return reject(err);
+      }
+      server = app.listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
+        resolve();
+      })
+        .on('error', err => {
+          mongoose.disconnect();
+          reject(err);
+        });
+    });
+  });
+}
+
+
+function closeServer() {
+  return mongoose.disconnect().then(() => {
+    return new Promise((resolve, reject) => {
+      console.log('Closing server');
+      server.close(err => {
+        if (err) {
+          return reject(err);
+        }
+        resolve();
+      });
+    });
+  });
+}
+
+if (require.main === module) {
+  runServer(DATABASE_URL).catch(err => console.error(err));
+}
+
+module.exports = {runServer, app, closeServer};
+
+
