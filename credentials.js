7faa8e5b075f4bda3fdc3063e731bc87c2ecf6a3
michelle barker <mbarker1221@gmail.com>
2018-02-18 06:00:11 -0500

    bug fixes

--- credentials.js
+++ credentials.js
@@ -8,24 +8,7 @@ mongo: {
 	production: {
 		connectionString: 'mongodb://mbarker1221:shompin1@ds131698.mlab.com:31698/users'
 	},
-},
-const mongoose = require('mongoose');
-const opts = {
-	server: {
-		socketOptions: {keepAlive:1}
-	}
-};
 
-switch(app.get('env')){
-	case 'developmet':
-	mongoose.connect(credentials.mongo.development.connectionString, opts);
-	break;
-	case 'production':
-	mongoose.connect(credentials.mongo.production.connectionString, opts);
-	break;
-	default:
-	throw new Error('Unknown execution environment:' + app.get('env'));
-}
 
 module.exports = {
 	cookieSecret: 'your cookie secret goes here',
