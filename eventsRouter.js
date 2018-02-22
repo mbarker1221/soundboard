6188490530183619fcf2d6ad633bab951e895d51
michelle barker <mbarker1221@gmail.com>
2018-02-22 05:45:11 -0500

    midnight oil push

--- eventsRouter.js
+++ eventsRouter.js
@@ -25,9 +25,11 @@ eventsRouter.get('/events', (req, res) => {
   const {title} = results.events.event.title,
   const {city_name} = results.events.event.city_name,
   const {start_time} = results.events.event.start_time, 
-  const {description} =  results.events.event,
+  const {description} =  results.events.event,description,
   const {venue_name} = results.events.event.venue_name,
   const {artist_name} = results.events.event.artist_name,
+  const {image} = results.events.event.image.small.url
+  })
   
 res.json(events.get());
 });
