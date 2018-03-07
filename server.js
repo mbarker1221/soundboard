2281f7b0c9fc497dcc7225dd63ca6e1dbddd48f8
michelle barker <mbarker1221@gmail.com>
2018-03-07 04:37:51 -0500

    updated package-lock modified files to incorporate tests

--- server.js
+++ server.js
@@ -1,34 +1,30 @@
 const express = require('express');
 const app = express();
 const {DATABASE_URL,PORT} = require('./config');
-const {User} = require ('./models');
+const {User} = require ('./userRouter');
 const {Event} = require ('./eventsRouter');
 const {Artist} = require ('./eventsRouter');
-
 const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-
 app.use(morgan('common'));
 app.use(bodyParser.json());
-
+//app.use(express.static('public'));
+//var $ = require ('jquery');
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/user/index.html');
+    res.sendFile(path.join(__dirname) + './public/index.html');
 });
-
-app.get('/user', (req,res) => {
-    res.json(User.get());
-});
-
+ 
 app.get('/user', (req, res) => {
   User
     .find()
-    .then(User => {
-        res.json(User.map(User => User.serialize()));
-    })
+    .then(user => {
+        res.json({user: user.map((user) => user.serialize())
+      });
+  })
     .catch(err => {
         console.error(err);
         res.status(500).json({
@@ -37,7 +33,16 @@ app.get('/user', (req, res) => {
     });
 });
 
-//create new user
+app.get('/user/id', (req, res) => {
+  User
+    .findById(req.params.id)
+    .then(user => res.json(user.serialize()))
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ error: 'something went horribly awry'});
+    });
+});
+
 app.post('/user', jsonParser, (req, res) => {
   const requiredFields = ['username', 'password', 'email'];
       for (let i=0; i<requiredFields.length; i++) {
@@ -52,50 +57,44 @@ app.post('/user', jsonParser, (req, res) => {
       .create({
         username: req.body.username, 
         password: req.body.password, 
-        email: req.body.email})
-        res.status(201).json(User);
+        email: req.body.email
+      })
+      .then(user => res.status(201).json(user.serialize()));
       });
 
 
 app.put('/user/:id', jsonParser, (req, res) => {
-  const requiredFields = ['username', 'password', 'email', 'id'];
-    for (let i=0; i<requiredFields.length; i++) {
-      const field = requiredFields[i];
-        if (!(field in req.body)) {
-      const message = `Missing \`${field}\` in request body`
-      console.error(message);
-        return res.status(400).send(message);
+
+ if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
+    const message = (
+      `Request path id (${req.params.id}) and request body id ` +
+      `(${req.body.id}) must match`);
+          console.error(message);
+        return res.status(400).json({ message: message });
       }
-    }
-
-    if (req.params.id !== req.body.id) {
-      const message = `Request path id (${req.params.id}) and request body id (${req.body.id}) must match`;
-      console.error(message);
-    return res.status(400).send(message);
-    }
-      console.log(`Updating User \`${req.params.id}\``);
-   User.update({
-    username: req.body.username,
-    password: req.body.password,
-    email: req.body.email
-  });
-    res.status(204).end();
-    });
-
-
-  app.delete('/user/:id', (req, res) => {
-    console.log(`Deleted User \`${req.params.id}\``);
+    const toUpdate = {};
+    const updateableFields = ['username', 'password', 'email'];
+      updateableFields.forEach(field => {
+        if (field in req.body) {
+          toUpdate[field] = req.body[field];
+          }
+      });
       User
-        .delete(
-          req.params.id),
-          res.status(201).json(User);
+        .findByIdAndUpdate(req.params.id, {$set: toUpdate})
+        .then(user => res.status(204).end())
+        .catch(err => res.status(500).json({message: 'Something went wrong'}));
     });
 
+  app.delete('/user/:id', function(req,res){
+    User
+    .findByIdAndRemove(req.params.id)
+    .then (user => res.status(204).end())
+    .catch(err => res.status(500).json({message: 'internal error'}));
+  });
 
   app.use('*', function(req, res) {
-      res.status(404).json({message: 'Not Found'})
+      res.status(408).json({message: 'No Found'})
   });
-
 
     let server;
 
--- userRouter.js
+++ userRouter.js
@@ -1,20 +1,45 @@
+const bcrypt = require('bcryptjs');
+
 const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
+const morgan = require('morgan');
+
+mongoose.Promise = global.Promise;
+
+app.use(morgan('common'));
+app.use(bodyParser.json());
 
 const UserSchema = mongoose.Schema({
-  username: {type: String, required: true},
+  username: {type: String, required: true, unique: true},
   password: {type: String, required: true},
   email: {type: String, required: true},
   });
 
 UserSchema.methods.serialize = function() {
-
   return {
-    id: this._id,
+    id: this.id,
     username: this.username,
     password: this.password,
     email: this.email
   };
-}
+};
+
+//UserSchema.methods.validatePassword = function(password) {
+//  return bcrypt.compare(password, this.password);
+//};
+
+//UserSchema.stastics.hashPassword = function(password) {
+// return bcrypt.hash(password, 10);
+//};
+
 const User = mongoose.model('User', UserSchema);
 
 module.exports = {User};
+
+
+
+ 
+
