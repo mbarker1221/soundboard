1d4cfea8c81080b84a11e1f12607251ba03b33ee
michelle barker <mbarker1221@gmail.com>
2018-04-09 18:06:26 -0400

    deleted files

--- authIndex.js
+++
@@ -1,7 +0,0 @@
-
-
-'use strict';
-const {router} = require('./authRouter');
-const {localStrategy, jwtStrategy} = require('./authStrategies');
-
-module.exports = {router, localStrategy, jwtStrategy};
