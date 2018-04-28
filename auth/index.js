8ffb913049fc6cfd321121877840092060b220bb
michelle barker <mbarker1221@gmail.com>
2018-04-28 01:07:39 -0400

    attempting to fix conflicts

--- auth/index.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-/*jshint esversion: 6 */
-/*jshint node: true;*/
-
-const {router} = require('./router');
-const {localStrategy, jwtStrategy} = require('./strategies');
-
-module.exports = {router, localStrategy, jwtStrategy};
