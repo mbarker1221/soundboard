1ffce0c015aeb46cf9a64bb2729879ac9eeb2b1e
michelle barker <mbarker1221@gmail.com>
2018-05-01 04:09:37 -0400

    issues with the cloud

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,39 +1,36 @@
 'use strict';
 /*jshint esversion: 6 */
-/*jshint node: true;*/
+/*jshint node: true */
 
 const express = require('express');
 const passport = require('passport');
 const bodyParser = require('body-parser');
-const {
-   User
-} = require('./models');
+const {User} = require('./models');
+const router = express.Router();
 
 const app = express();
 const config = require('../config');
-const router = express.Router();
 const jsonParser = bodyParser.json();
 
 app.use(bodyParser.json());
-
 
 router.post('/', jsonParser, (req, res) => {
    const requiredFields = ["username", "password", "email"];
    const missingField = requiredFields.find(field => !(field in req.body));
 
    if (missingField) {
-      return res.status(422).json({
-         code: 422,
+     return res.status(422).json({
+        code: 422,
          reason: "ValidationError",
-         message: "Missing field",
-         location: missingField
+        message: "Missing field",
+        location: missingField
       });
    }
 
-   const stringFields = ["username", "password", "email"];
-   const nonStringField = stringFields.find(
-      field => field in req.body && typeof req.body[field] !== "string"
-   );
+const stringFields = ["username", "password", "email"];
+const nonStringField = stringFields.find(
+   field => field in req.body && typeof req.body[field] !== "string"
+);
 
    if (nonStringField) {
       return res.status(422).json({
@@ -98,15 +95,9 @@ router.post('/', jsonParser, (req, res) => {
       });
    }
 
-   let {
-      username,
-      password,
-      email
-   } = req.body;
+   let {username, password, email} = req.body;
 
-   return User.find({
-         username
-      })
+   return User.find({username})
       .count()
       .then(count => {
          if (count > 0) {
@@ -137,11 +128,7 @@ router.post('/', jsonParser, (req, res) => {
             return res.status(err.code).json(err);
          }
 
-
-         res.status(500).json({
-            code: 500,
-            message: 'Internal server error'
-         });
+         res.status(500).json({code: 500, message: 'Internal server error'});
       });
 });
 //router.get('/', (req, res) => {
@@ -154,6 +141,111 @@ router.get('/', (req, res) => {
     .catch(err => res.status(500).json({message: 'Internal server error'}));
 });
 */
-module.exports = {
-   router
-};
+
+router.get('/user', (req, res) => {
+    const filters = {};
+    const queryableFields = ["username", "password"];
+    queryableFields.forEach(field => {
+        if (req.query[field]) {
+            filters[field] = req.query[field];
+        }
+    });
+    User
+        .find(filters)
+        .then(User => res.json(
+            User.map(user => user.serialize())
+        ))
+        .catch(err => {
+            console.error(err);
+            res.status(500).json({message: "did not find user"});
+        });
+});
+
+app.get('/user/:id', (req,res) => {
+  const {userId} = req.params;
+});
+/*
+app.post('/user', (req, res) => {
+  const requiredFields = ['username', 'password', 'email'];
+
+  for (let i = 0; i < requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`;
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+
+  User
+    .create({
+     username: req.body.username,
+     password: req.body.password,
+     email: req.body.email
+    })
+    .then(user => res.status(201).json(user.serialize()))
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({message: 'Did not create'});
+    });
+});
+*/
+
+router.put('/users/:id', (req, res) => {
+  if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
+    const message = (
+      `Request path id (${req.params.id}) and request body id ` +
+      `(${req.body.id}) must match`);
+    console.error(message);
+    return res.status(400).json({message: message});
+  }
+  const toUpdate = {};
+  const updateableFields = ['username', 'password'];
+
+  updateableFields.forEach(field => {
+    if (field in req.body) {
+      toUpdate[field] = req.body[field];
+    }
+  });
+
+  User
+    .findByIdAndUpdate(req.params.id, {$set: toUpdate})
+    .then(user => res.status(204).end())
+    .catch(err => res.status(500).json({message: 'Did not update'}));
+});
+/*
+KEEP THIS!!!!
+const userData = [
+{
+  username: 'username',
+  password: 'password',
+  email: 'email@email.com'
+}
+];
+// the user would actually make a request
+// to one of the IDs, like `/9920711`. `studentId`
+// is accessible in the `req.params` object.
+app.get('/:userId', (req, res) => {
+  // use destructuring assignment to adsign `req.params.studentId`
+  // to its own variable
+  const {userId} = req.params;
+  let requestedData;
+  // loop through studentData td find a matching studentId
+  for (let i = 0; i<userData.length; i++) {
+    if (userData[i].userId === userId) {
+      requestedData = userData[i]
+    }
+  };
+  // send the data matchdng the requested studentId
+  res.json(requestedData);
+});
+*/
+
+router.delete('/users/:id', (req, res) => {
+  User
+    .findByIdAndRemove(req.params.id)
+    .then(user => res.status(204).end())
+    .catch(err => res.status(500).json({message: 'Did not delete'}));
+});
+
+module.exports = {router};
